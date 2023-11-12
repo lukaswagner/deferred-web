3d14ccf531b99b9794b5d35445ce54b00c4212b8
Lukas Wagner <lukas@lwgnr.dev>
2023-11-12 15:26:15 +0100

    refactor app, add fullscreen

--- src/app.ts
+++ src/app.ts
@@ -4,38 +4,60 @@ import { Navigation } from './util/navigation';
 import { UI } from '@lukaswagner/web-ui';
 import { createDebugScene } from './scene';
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const contextAttributes: WebGLContextAttributes = { antialias: false, alpha: false };
-const gl = canvas.getContext('webgl2', contextAttributes);
-if (!gl) throw new Error('Could not acquire WebGL context');
+class App {
+    protected _canvas: HTMLCanvasElement;
+    protected _renderer: Renderer;
 
-const camera = new Camera();
-new Navigation(canvas, camera);
+    constructor(canvasId: string, uiId: string, containerId?: string) {
+        this._canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+        const contextAttributes: WebGLContextAttributes = { antialias: false, alpha: false };
+        const gl = this._canvas.getContext('webgl2', contextAttributes);
+        if (!gl) throw new Error('Could not acquire WebGL context');
 
-const renderer = new Renderer(gl);
-renderer.camera = camera;
-renderer.initialize();
-setupUI();
-const scene = createDebugScene(gl);
-renderer.scene = scene;
+        const camera = new Camera();
+        new Navigation(this._canvas, camera);
 
-function drawLoop(time: number) {
-    const shouldDraw = renderer.prepare();
-    if (shouldDraw) renderer.draw(time);
-    requestAnimationFrame(drawLoop);
+        this._renderer = new Renderer(gl);
+        this._renderer.camera = camera;
+        this._renderer.initialize();
+
+        const scene = createDebugScene(gl);
+        this._renderer.scene = scene;
+
+        this.setupUI(uiId);
+        this.setupFullscreen(containerId ?? canvasId);
+
+        requestAnimationFrame((t) => this.draw(t));
+    }
+
+    protected setupUI(uiId: string) {
+        const element = document.getElementById(uiId) as HTMLCanvasElement;
+        const ui = new UI(element, true);
+
+        const debugViews = this._renderer.getDebugViews();
+        ui.input.select({
+            label: 'output',
+            optionValues: debugViews.map((d) => d.name),
+            handler: (v) => this._renderer.setDebugView(debugViews[v.index]),
+            handleOnInit: false,
+        });
+    }
+
+    protected setupFullscreen(id: string) {
+        const element = document.getElementById(id);
+        element.ondblclick = () => {
+            if(document.fullscreenElement === element)
+                document.exitFullscreen();
+            else
+                element.requestFullscreen();
+        }
+    }
+
+    protected draw(time: number) {
+        const shouldDraw = this._renderer.prepare();
+        if (shouldDraw) this._renderer.draw(time);
+        requestAnimationFrame((t) => this.draw(t));
+    }
 }
-requestAnimationFrame(drawLoop);
 
-function setupUI() {
-    const uiId = 'ui';
-    const element = document.getElementById(uiId) as HTMLCanvasElement;
-    const ui = new UI(element, true);
-
-    const debugViews = renderer.getDebugViews();
-    ui.input.select({
-        label: 'output',
-        optionValues: debugViews.map((d) => d.name),
-        handler: (v) => renderer.setDebugView(debugViews[v.index]),
-        handleOnInit: false,
-    });
-}
+new App('canvas', 'ui', 'container');
