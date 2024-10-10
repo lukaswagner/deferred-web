025a3e5e3b2f7d15cba2ffa5934b5588e2c195d0
Lukas Wagner <lukas@lwgnr.dev>
2024-10-10 12:31:46 +0200

    use renderer class

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,4 @@
-import { Camera, CanvasFramebuffer, Dirty, drawBuffers, TextureFormats, Framebuffer, halton2d, Texture2D, TextureFormat, AccumulatePass, BlitPass, isCameraPass, isJitterPass, RenderPass } from '@lukaswagner/webgl-toolkit';
+import { Camera, CanvasFramebuffer, Dirty, drawBuffers, TextureFormats, Framebuffer, halton2d, Texture2D, TextureFormat, AccumulatePass, BlitPass, isCameraPass, isJitterPass, RenderPass, TaaRenderer } from '@lukaswagner/webgl-toolkit';
 import { GeometryPass, FragmentLocation as GeomLocations } from './passes/geometryPass';
 import { mat4, vec2 } from 'gl-matrix';
 import { Geometry } from './geometry/geometry';
@@ -24,23 +24,12 @@ type DebugView = {
     buffer: number,
 }
 
-export class Renderer {
-    protected _gl: WebGL2RenderingContext;
-    protected _canvas: HTMLCanvasElement;
-    protected _resizeObserver: ResizeObserver;
-    protected _size: vec2;
-    protected _sizeFactor = 1;
-
-    protected _dirty = new Dirty(TrackedMembers);
-    protected _camera: Camera;
-    protected _lastFrame = 0;
+export class Renderer extends TaaRenderer<typeof TrackedMembers> {
     protected _scene: Scene;
     protected _updateScene = true;
 
-    protected _framebuffers: Framebuffer[] = [];
     protected _accumulateBuffer: Framebuffer;
 
-    protected _passes: RenderPass<any>[] = [];
     protected _geometryPass: GeometryPass;
     protected _ambientLightPass: AmbientLightPass;
     protected _directionalLightPass: DirectionalLightPass;
@@ -48,16 +37,8 @@ export class Renderer {
     protected _accumulatePass: AccumulatePass;
     protected _blitPass: BlitPass;
 
-    protected _taaEnabled = true;
-    protected _taaFrame = 0;
-    protected _taaNumFrames = 64;
-    protected _taaHaltonBase1 = 2;
-    protected _taaHaltonBase2 = 3;
-    protected _taaKernel: vec2[];
-
     public constructor(gl: WebGL2RenderingContext) {
-        this._gl = gl;
-        this._canvas = gl.canvas as HTMLCanvasElement;
+        super(gl, TrackedMembers);
     }
 
     public initialize() {
@@ -69,52 +50,52 @@ export class Renderer {
         const canvasFbo = CanvasFramebuffer.getInstance(this._gl);
         this._framebuffers.push(canvasFbo);
 
-        const geom = this.setupGeometryBuffer();
-        this.setupGeometryPass(geom.fbo);
+        const geom = this._setupGeometryBuffer();
+        this._setupGeometryPass(geom.fbo);
 
-        const ambient = this.setupSingleChannelBuffer('Ambient Light', TextureFormats.RGBA16F);
+        const ambient = this._setupSingleTexBuffer('Ambient Light', TextureFormats.RGBA16F);
         this._ambientLightPass = new AmbientLightPass(this._gl, 'Ambient Light');
-        this.setupLightPass(this._ambientLightPass, ambient.fbo);
+        this._setupLightPass(this._ambientLightPass, ambient.fbo);
 
-        const directional = this.setupSingleChannelBuffer('Directional Light', TextureFormats.RGBA16F);
+        const directional = this._setupSingleTexBuffer('Directional Light', TextureFormats.RGBA16F);
         this._directionalLightPass = new DirectionalLightPass(this._gl, 'Directional Light');
-        this.setupLightPass(this._directionalLightPass, directional.fbo);
+        this._setupLightPass(this._directionalLightPass, directional.fbo);
         this._directionalLightPass.normal = geom.normal;
 
-        const point = this.setupSingleChannelBuffer('Point Light', TextureFormats.RGBA16F);
-        this.setupPointLightPass(point.fbo, geom.position, geom.normal);
+        const point = this._setupSingleTexBuffer('Point Light', TextureFormats.RGBA16F);
+        this._setupPointLightPass(point.fbo, geom.position, geom.normal);
 
-        const merge = this.setupSingleChannelBuffer('Light Merge');
-        this.setupLightMergePass(merge.fbo, ambient.texture, directional.texture, point.texture, geom.color);
+        const merge = this._setupSingleTexBuffer('Light Merge');
+        this._setupLightMergePass(merge.fbo, ambient.texture, directional.texture, point.texture, geom.color);
 
-        const accumulate = this.setupSingleChannelBuffer('TAA');
-        this._accumulateBuffer = accumulate.fbo;
-        this.setupAccumulatePass(accumulate.fbo, merge.texture);
+        const taa = this._setupTaa(merge.texture);
+        this._accumulateBuffer = taa.buffer.fbo;
+        this._accumulatePass = taa.pass;
+        this._passes.push(this._accumulatePass);
 
-        this.setupBlitPass(
-            accumulate.fbo, this._gl.COLOR_ATTACHMENT0 + LightLocations.Color,
+        this._blitPass = this._setupBlitPass(
+            this._accumulateBuffer, this._gl.COLOR_ATTACHMENT0 + LightLocations.Color,
             canvasFbo, this._gl.BACK
         );
+        this._passes.push(this._blitPass);
 
-        this._resize();
-        this._watchResize();
-
+        super.initialize();
         this._gl.enable(this._gl.DEPTH_TEST);
     }
 
-    private setupGeometryBuffer() {
+    private _setupGeometryBuffer() {
         const fbo = new Framebuffer(this._gl, 'Forward');
         const c0 = this._gl.COLOR_ATTACHMENT0;
 
-        const color = this.createTex(TextureFormats.RGBA);
+        const color = this._createTex(TextureFormats.RGBA);
         color.minFilter = this._gl.NEAREST;
         color.magFilter = this._gl.NEAREST;
 
-        const position = this.createTex(TextureFormats.RGBA16F);
+        const position = this._createTex(TextureFormats.RGBA16F);
         position.minFilter = this._gl.NEAREST;
         position.magFilter = this._gl.NEAREST;
 
-        const normal = this.createTex(TextureFormats.RGBA16F);
+        const normal = this._createTex(TextureFormats.RGBA16F);
         normal.minFilter = this._gl.NEAREST;
         normal.magFilter = this._gl.NEAREST;
 
@@ -122,15 +103,15 @@ export class Renderer {
             { slot: c0 + GeomLocations.Color, texture: color },
             { slot: c0 + GeomLocations.WorldPosition, texture: position },
             { slot: c0 + GeomLocations.WorldNormal, texture: normal },
-            { slot: c0 + GeomLocations.ViewPosition, texture: this.createTex(TextureFormats.RGBA16F) },
-            { slot: c0 + GeomLocations.ViewNormal, texture: this.createTex(TextureFormats.RGBA16F) },
-            { slot: this._gl.DEPTH_ATTACHMENT, texture: this.createTex(TextureFormats.Depth) },
+            { slot: c0 + GeomLocations.ViewPosition, texture: this._createTex(TextureFormats.RGBA16F) },
+            { slot: c0 + GeomLocations.ViewNormal, texture: this._createTex(TextureFormats.RGBA16F) },
+            { slot: this._gl.DEPTH_ATTACHMENT, texture: this._createTex(TextureFormats.Depth) },
         ]);
         this._framebuffers.push(fbo);
         return { fbo, color, position, normal };
     }
 
-    private setupGeometryPass(target: Framebuffer) {
+    private _setupGeometryPass(target: Framebuffer) {
         const pass = new GeometryPass(this._gl, 'Geometry Forward');
         pass.initialize();
         pass.target = target;
@@ -142,20 +123,7 @@ export class Renderer {
         this._geometryPass = pass;
     }
 
-    private setupSingleChannelBuffer(name: string, format = TextureFormats.RGBA) {
-        const fbo = new Framebuffer(this._gl, name);
-        const texture = this.createTex(format);
-        texture.minFilter = this._gl.NEAREST;
-        texture.magFilter = this._gl.NEAREST;
-        const c0 = this._gl.COLOR_ATTACHMENT0;
-        fbo.initialize([
-            { slot: c0 + LightLocations.Color, texture },
-        ]);
-        this._framebuffers.push(fbo);
-        return { fbo, texture };
-    }
-
-    private setupLightPass(pass: BaseLightPass, target: Framebuffer) {
+    private _setupLightPass(pass: BaseLightPass, target: Framebuffer) {
         pass.initialize();
         pass.target = target;
 
@@ -163,7 +131,7 @@ export class Renderer {
         this._passes.push(pass);
     }
 
-    private setupPointLightPass(target: Framebuffer, position: Texture2D, normal: Texture2D) {
+    private _setupPointLightPass(target: Framebuffer, position: Texture2D, normal: Texture2D) {
         const pass = new PointLightPass(this._gl, 'Directional Light');
 
         pass.initialize();
@@ -186,7 +154,7 @@ export class Renderer {
         this._pointLightPass = pass;
     }
 
-    private setupLightMergePass(
+    private _setupLightMergePass(
         target: Framebuffer,
         ambient: Texture2D, directional: Texture2D, point: Texture2D,
         color: Texture2D
@@ -203,160 +171,17 @@ export class Renderer {
         this._passes.push(pass);
     }
 
-    private setupAccumulatePass(target: Framebuffer, input: Texture2D) {
-        const pass = new AccumulatePass(this._gl, 'TAA Accumulate');
-        pass.initialize();
-        pass.target = target;
-
-        input.minFilter = this._gl.NEAREST;
-        input.magFilter = this._gl.NEAREST;
-        pass.input = input;
-
-        pass.preDraw = () => {
-            this._gl.enable(this._gl.BLEND);
-            this._gl.blendFunc(this._gl.SRC_ALPHA, this._gl.ONE_MINUS_SRC_ALPHA);
-        };
-        pass.postDraw = () => {
-            this._gl.disable(this._gl.BLEND);
-        };
-
-        this._passes.push(pass);
-        this._accumulatePass = pass;
-    }
-
-    private setupBlitPass(srcFbo: Framebuffer, srcBuffer: GLenum, dstFbo: Framebuffer, dstBuffer: GLenum) {
-        const pass = new BlitPass(this._gl);
-        pass.readTarget = srcFbo;
-        pass.readBuffer = srcBuffer;
-        pass.drawTarget = dstFbo;
-        pass.drawBuffer = dstBuffer;
-        this._passes.push(pass);
-        this._blitPass = pass;
-    }
-
-    protected createTex(format: TextureFormat) {
-        const tex = new Texture2D(this._gl);
-        tex.initialize(format);
-        return tex;
-    }
-
     public prepare(time: number): boolean {
-        let shouldRun = this._dirty.any();
-
-        let sceneChange = SceneChange.None;
+        let sceneChanged = false;
         if(this._updateScene) {
-            sceneChange = this._scene.update(time);
-            if(sceneChange !== SceneChange.None) shouldRun = true;
+            const sceneChange = this._scene.update(time);
+            if(sceneChange !== SceneChange.None) sceneChanged = true;
             this._applyScene(sceneChange);
         }
 
-        if (this._dirty.get('Size')) {
-            this._gl.viewport(0, 0, this._size[0], this._size[1]);
-            for (const fbo of this._framebuffers) {
-                fbo.size = this._size;
-            }
-        }
+        this._prepareTaa(sceneChanged, this._accumulateBuffer, this._accumulatePass);
 
-        const cameraChanged = this._camera.timestamp > this._lastFrame;
-        if (this._taaEnabled) {
-            const taaSettingsChanged =
-                this._dirty.get('TaaEnabled') ||
-                this._dirty.get('TaaNumFrames') ||
-                this._dirty.get('TaaHaltonBase1') ||
-                this._dirty.get('TaaHaltonBase2');
-            if(taaSettingsChanged) {
-                this._taaKernel = halton2d(
-                    this._taaHaltonBase1,
-                    this._taaHaltonBase2,
-                    this._taaNumFrames);
-            }
-
-            if(taaSettingsChanged || cameraChanged || sceneChange !== SceneChange.None)
-            {
-                this._taaFrame = 0;
-                this._dirty.set('TaaFrame');
-                this._accumulateBuffer.clear();
-            }
-
-            if(this._dirty.get('TaaFrame')) {
-                let ndcOffset = this._taaFrame === 0 ?
-                    vec2.create() :
-                    vec2.clone(this._taaKernel[this._taaFrame - 1]);
-
-                // loop around at 0.5 to -0.5
-                if(ndcOffset[0] > 0.5) ndcOffset[0] -= 1;
-                if(ndcOffset[1] > 0.5) ndcOffset[1] -= 1;
-
-                for (const pass of this._passes) {
-                    if (isJitterPass(pass)) {
-                        pass.ndcOffset = ndcOffset;
-                    }
-                }
-
-                this._accumulatePass.frame = this._taaFrame;
-            }
-        }
-
-        if (cameraChanged) {
-            const view = this._camera.view;
-            const projection = this._camera.projection;
-            const viewProjection = mat4.mul(mat4.create(), projection, view);
-            const viewInverse = mat4.invert(mat4.create(), view);
-            const projectionInverse = mat4.invert(mat4.create(), projection);
-            const viewProjectionInverse = mat4.invert(mat4.create(), viewProjection);
-            for (const pass of this._passes) {
-                if (isCameraPass(pass)) {
-                    pass.view = view;
-                    pass.projection = projection;
-                    pass.viewProjection = viewProjection;
-                    pass.viewInverse = viewInverse;
-                    pass.projectionInverse = projectionInverse;
-                    pass.viewProjectionInverse = viewProjectionInverse;
-                }
-            }
-            shouldRun = true;
-        }
-
-        for (const pass of this._passes) {
-            if (pass.prepare()) shouldRun = true;
-        }
-
-        this._dirty.reset();
-        return shouldRun;
-    }
-
-    public draw(time: number) {
-        for (const pass of this._passes) {
-            pass.draw();
-        }
-
-        this._lastFrame = time;
-        if(this._taaEnabled) {
-            this._taaFrame++;
-            if(this._taaFrame < this._taaNumFrames)
-                this._dirty.set('TaaFrame');
-        }
-    }
-
-    protected _resize() {
-        const newSize = vec2.fromValues(
-            Math.floor(this._canvas.clientWidth * window.devicePixelRatio * this._sizeFactor),
-            Math.floor(this._canvas.clientHeight * window.devicePixelRatio * this._sizeFactor)
-        );
-
-        this._size = newSize;
-        this._camera.aspect = this._size[0] / this._size[1];
-        this._dirty.set('Size');
-    }
-
-    protected _watchResize() {
-        this._resizeObserver =
-            new ResizeObserver(this._resize.bind(this) as ResizeObserverCallback);
-        this._resizeObserver.observe(this._canvas);
-    }
-
-    public set camera(v: Camera) {
-        this._camera = v;
+        return super.prepare(time) || sceneChanged;
     }
 
     public set scene(v: Scene) {
@@ -423,31 +248,5 @@ export class Renderer {
     public set debugView(v: DebugView) {
         this._blitPass.readTarget = v.target;
         this._blitPass.readBuffer = v.buffer;
-    }
-
-    public set sizeFactor(v: number) {
-        this._sizeFactor = v;
-        this._resize();
-    }
-
-    public set taaEnabled(v: boolean) {
-        this._taaEnabled = v;
-        this._dirty.set('TaaEnabled');
-    }
-
-    public set taaNumFrames(v: number) {
-        this._taaNumFrames = v;
-        this._dirty.set('TaaNumFrames');
-    }
-
-    public set taaHaltonSequence(v: [number, number]) {
-        this._taaHaltonBase1 = v[0];
-        this._dirty.set('TaaHaltonBase1');
-        this._taaHaltonBase2 = v[1];
-        this._dirty.set('TaaHaltonBase2');
-    }
-
-    public get taaFrame() {
-        return this._taaFrame;
     }
 }
